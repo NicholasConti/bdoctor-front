d1e3f56c5192b3b42c44fedb0230c9e4ed417f33
NicholasConti <conti94@live.it>
2023-06-12 17:22:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import HelloApp from './components/HelloApp.vue'
+export default {
+    name: 'App',
+    components: {
+        HelloApp
+    }
+}
+</script>
+
+<template>
+    <HelloApp />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/HelloApp.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'HelloApp'
+}
+</script>
+
+<template>
+    <h1 class="text-center">Ciaoooooooo</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+    color: blue
+}
+</style>
