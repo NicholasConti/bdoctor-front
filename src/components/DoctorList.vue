bdc5b169c5d806633911eebbd78be8f1350d378c
NicholasConti <conti94@live.it>
2023-06-13 18:05:02 +0200

    cards-structure

--- src/components/DoctorList.vue
+++ src/components/DoctorList.vue
@@ -13,10 +13,27 @@ export default {
                 <span class="input-group-text" id="basic-addon1">Search</span>
             </div>
         </form>
-        <div>
-
+        <div class="d-flex justify-content-center p-2 gap-3">
+            <button class="btn btn-primary">specializ1</button>
+            <button class="btn btn-primary">specializ2</button>
+            <button class="btn btn-primary">specializ3</button>
+            <button class="btn btn-primary">specializ4</button>
+            <button class="btn btn-primary">specializ5</button>
         </div>
-        <router-link :to="{ name: 'InfoDoctor' }">vai in pace</router-link>
+        <div class="row">
+            <div class="col-lg-3 col-sm-6 col-md-4">
+                <div class="card">
+                    <img src="../../public/image/doct1.jpg" class="card-img-top" alt="di">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                            card's content.</p>
+                        <router-link :to="{ name: 'InfoDoctor' }">vai in pace</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </template>
 
