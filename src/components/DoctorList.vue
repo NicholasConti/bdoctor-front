7a32d5b0ff081263558d25e3955ed344771fea16
NicholasConti <conti94@live.it>
2023-06-13 17:43:12 +0200

    search-bar

--- src/components/DoctorList.vue
+++ src/components/DoctorList.vue
@@ -5,8 +5,19 @@ export default {
 </script>
 
 <template>
-    <h1>Doctors</h1>
-    <router-link :to="{name:'InfoDoctor'}">vai in pace</router-link>
+    <div class="container">
+        <h1 class="text-center">h1 con bella scritta</h1>
+        <form action="" method="get">
+            <div class="input-group mb-3 w-50 m-auto">
+                <input type="text" class="form-control" aria-describedby="basic-addon1">
+                <span class="input-group-text" id="basic-addon1">Search</span>
+            </div>
+        </form>
+        <div>
+
+        </div>
+        <router-link :to="{ name: 'InfoDoctor' }">vai in pace</router-link>
+    </div>
 </template>
 
 <style lang="scss" scoped>
