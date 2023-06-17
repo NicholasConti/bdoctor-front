237f35041678cc7b44a236f63ee5bf9a7dc17abd
daniloeberli <danilo.eberli@gmail.com>
2023-06-17 11:33:47 +0200

    feat: error 404 page

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: "NotFoundPage"
+}
+</script>
+
+<template>
+    <div class="d-flex flex-row align-items-center">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12 text-center">
+                    <span class="display-1 d-block">404</span>
+                    <div class="mb-4 lead">The page you are looking for was not found.</div>
+                    <router-link class="btn btn-primary" :to="{name: home}">Back to Home</router-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+body {
+    background: #dedede;
+}
+
+.page-wrap {
+    min-height: 100vh;
+}
+</style>
